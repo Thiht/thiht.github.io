5ece1dcc3da90427ac546beb8ed6116f796e0104
Thibaut Rousseau <contact@thibaut-rousseau.com>
2024-12-19 22:29:21 +0100

    style improvements

--- content/blog/_index.md
+++ content/blog/_index.md
@@ -1,7 +1,6 @@
 +++
 title = "List of blog posts"
 sort_by = "date"
-template = "blog.html"
 page_template = "blog-page.html"
-render = false
+render = false # Don't render /blog
 +++
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,39 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html", "./content/**/*.md", "./static/icons/**/*.svg"],
+  content: [
+    "./templates/**/*.html",
+    "./content/**/*.md",
+    "./static/icons/**/*.svg",
+  ],
   theme: {
-    fontFamily: {
-      'cursive': ['"Charm"', 'cursive'],
-    },
     extend: {
+      fontFamily: {
+        sans: ['"IBM Plex Sans"', ...defaultTheme.fontFamily.sans],
+        cursive: ['Charm', "cursive"],
+      },
       typography: {
         DEFAULT: {
           css: {
-            'code': {
+            code: {
               // TODO: reference values from theme
-              backgroundColor: '#f5f5f5',
-              padding: '0.2em 0.4em',
-              borderRadius: '0.25em',
-              color: '#333',
-              fontWeight: 'normal',
+              backgroundColor: "#f5f5f5",
+              padding: "0.2em 0.4em",
+              borderRadius: "0.25em",
+              color: "#333",
+              fontWeight: "normal",
             },
-            'code::before': {
-              content: '""'
+            "code::before": {
+              content: '""',
             },
-            'code::after': {
-              content: '""'
-            }
-          }
-        }
+            "code::after": {
+              content: '""',
+            },
+          },
+        },
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
